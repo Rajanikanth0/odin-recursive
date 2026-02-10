cce1e43bd6011ae2de5d27897be7935eb93827a7
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2026-02-10 10:45:33 +0530

    add fibonacci test and impl (recursive)

--- src/modules/fibonacci.js
+++ src/modules/fibonacci.js
@@ -14,4 +14,22 @@ const fibs = (n) => {
   return arr;
 };
 
-export { fibs };
+const fibsRec = (n) => {
+  if (typeof n !== "number" || !Number.isInteger(n) || n < 0) {
+    throw new TypeError("fibsRec() requires a non-negative integer");
+  }
+
+  return fib(n);
+
+  function fib(n) {
+    if (n == 0) return [];
+    if (n == 1) return [0];
+    if (n == 2) return [0, 1];
+    
+    const arr = fibsRec(n-1);
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    return arr;
+  }
+}
+
+export { fibs, fibsRec };
--- src/modules/fibonacci.test.js
+++ src/modules/fibonacci.test.js
@@ -1,19 +1,25 @@
 import * as fibonacci from "./fibonacci.js";
 
-describe("fibs", () => {
-  describe("throws error on invalid input", () => {
+const entries = Object.entries(fibonacci);
+
+describe.each(entries)("[Function: %s]", (name, fn) => {
+  describe("invalid input handling", () => {
     const invalids = [null, "string", undefined, -5];
 
-    test.each(invalids)("invalid input: %s", (input) => {
-      expect(() => fibonacci.fibs(input)).toThrow(TypeError);
+    test.each(invalids)("throws TypeError for %s", (input) => {
+      expect(() => fn(input)).toThrow(TypeError);
     });
   });
 
-  test("returns an array of numbers", () => {
-    expect(fibonacci.fibs(5)).toEqual(expect.arrayOf(expect.any(Number)));
-  });
-  
-  test("returns fibonacci series upto n", () => {
-    expect(fibonacci.fibs(5)).toEqual([0, 1, 1, 2, 3]);
+  describe("valid input handling", () => {
+    test("returns an array of numbers", () => {
+      expect(fn(5)).toEqual(expect.any(Array));
+      // expect(fn(5).every(item => typeof item === "number")).toBe(true);
+      expect(fn(5)).toEqual(expect.arrayOf(expect.any(Number)));
+    });
+    
+    test("returns Fibonacci series of length n", () => {
+      expect(fn(5)).toEqual([0, 1, 1, 2, 3]);
+    });
   });
 });
