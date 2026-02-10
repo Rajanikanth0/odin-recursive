3d4ccf5993d7215656c820b158b9ecdc638d294e
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2026-02-10 10:49:13 +0530

    test fibonacci fn with various length

--- src/modules/fibonacci.test.js
+++ src/modules/fibonacci.test.js
@@ -12,14 +12,24 @@ describe.each(entries)("[Function: %s]", (name, fn) => {
   });
 
   describe("valid input handling", () => {
+    const nLength = [
+      [0, []],
+      [1, [0]],
+      [2, [0, 1]],
+      [5, [0, 1, 1, 2, 3]],
+      [17, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]]
+    ]
+
     test("returns an array of numbers", () => {
       expect(fn(5)).toEqual(expect.any(Array));
       // expect(fn(5).every(item => typeof item === "number")).toBe(true);
       expect(fn(5)).toEqual(expect.arrayOf(expect.any(Number)));
     });
-    
-    test("returns Fibonacci series of length n", () => {
-      expect(fn(5)).toEqual([0, 1, 1, 2, 3]);
+
+    describe("Fibonacci series of length", () => {
+      test.each(nLength)("length of %s", (n, expected) => {
+        expect(fn(n)).toEqual(expected);
+      });
     });
   });
 });
